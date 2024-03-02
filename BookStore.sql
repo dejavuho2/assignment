3c6e2af2813476ba2c72f2c0259f402d4e9b052e
dejavuho2 <155792560+dejavuho2@users.noreply.github.com>
2024-03-02 16:40:08 +0700

    Create BookStore.sql

---
+++ BookStore.sql
@@ -0,0 +1,208 @@
+﻿create database MyShop
+USE [MyShop]
+GO
+
+/****** Object:  Table [dbo].[bill]    Script Date: 09/11/2021 11:19:43 CH ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[bill](
+	[bill_id] [bigint] IDENTITY(1,1) NOT NULL,
+	[user_id] [bigint] NOT NULL,
+	[total] [money] NOT NULL,
+	[payment] [varchar](250) NOT NULL,
+	[address] [ntext] NOT NULL,
+	[date] [date] NOT NULL,
+	[phone] [bigint] NOT NULL,
+ CONSTRAINT [PK_bill] PRIMARY KEY CLUSTERED 
+(
+	[bill_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[bill_detail]    Script Date: 09/11/2021 11:19:43 CH ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[bill_detail](
+	[detail_id] [bigint] IDENTITY(1,1) NOT NULL,
+	[bill_id] [bigint] NOT NULL,
+	[product_id] [varchar](100) NOT NULL,
+	[quantity] [int] NOT NULL,
+	[price] [money] NOT NULL,
+ CONSTRAINT [PK_bill_detail] PRIMARY KEY CLUSTERED 
+(
+	[detail_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[cart]    Script Date: 09/11/2021 11:19:43 CH ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[cart](
+	[cart_id] [bigint] NOT NULL,
+	[product_id] [varchar](100) NOT NULL,
+	[product_name] [varchar](255) NOT NULL,
+	[product_img] [varchar](255) NOT NULL,
+	[product_price] [float] NOT NULL,
+	[total] [float] NOT NULL,
+	[quantity] [int] NOT NULL,
+ CONSTRAINT [PK_cart] PRIMARY KEY CLUSTERED 
+(
+	[cart_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[category]    Script Date: 09/11/2021 11:19:43 CH ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[category](
+	[category_id] [int] IDENTITY(1,1) NOT NULL,
+	[category_name] [varchar](255) NOT NULL,
+ CONSTRAINT [PK_category] PRIMARY KEY CLUSTERED 
+(
+	[category_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[product]    Script Date: 09/11/2021 11:19:43 CH ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[product](
+	[product_id] [varchar](100) NOT NULL,
+	[category_id] [int] NOT NULL,
+	[product_name] [nvarchar](max) NOT NULL,
+	[product_price] [money] NOT NULL,
+	[product_describe] [nvarchar](max) NOT NULL,
+	[quantity] [int] NOT NULL,
+	[img] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_product] PRIMARY KEY CLUSTERED 
+(
+	[product_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[users]    Script Date: 09/11/2021 11:19:43 CH ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[users](
+	[user_id] [bigint] IDENTITY(1,1) NOT NULL,
+	[user_name] [nvarchar](200) NULL,
+	[user_email] [varchar](255) NOT NULL,
+	[user_pass] [nvarchar](255) NOT NULL,
+	[isAdmin] [nvarchar](50) NULL,
+ CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[bill] ON 
+
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (45, 5, 648000.0000, N'MOMO', N'Hà Đông, Hà Nội', CAST(N'2021-11-04' AS Date), 919208356)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (46, 5, 298000.0000, N'MOMO', N'Ninh Bình', CAST(N'2021-11-04' AS Date), 888195313)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (47, 5, 378000.0000, N'VNPAY', N'Ninh Bình', CAST(N'2021-11-03' AS Date), 88195313)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (48, 5, 270000.0000, N'Chua thanh toán (VNPAY)', N'Ninh Bình', CAST(N'2021-11-04' AS Date), 383298183)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (49, 5, 417000.0000, N'Chua thanh toán (VNPAY)', N'Ninh Bình', CAST(N'2021-11-04' AS Date), 888195313)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (50, 5, 567000.0000, N'COD', N'Ninh Bình', CAST(N'2021-11-04' AS Date), 888195313)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (51, 5, 278000.0000, N'COD', N'test', CAST(N'2021-11-04' AS Date), 123)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (52, 5, 189000.0000, N'COD', N'è', CAST(N'2021-11-04' AS Date), 2)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (53, 5, 259000.0000, N'MOMO', N'Check', CAST(N'2021-11-04' AS Date), 888195313)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (54, 5, 270000.0000, N'VNPAY', N'Check', CAST(N'2021-11-04' AS Date), 888195313)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (55, 5, 259000.0000, N'MOMO', N'jrtn', CAST(N'2021-11-06' AS Date), 2)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (56, 5, 810000.0000, N'VNPAY', N'df', CAST(N'2021-11-06' AS Date), 3)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (57, 5, 149000.0000, N'MOMO', N'Ninh Binh', CAST(N'2021-11-08' AS Date), 383298183)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (58, 5, 417000.0000, N'VNPAY', N'bf', CAST(N'2021-11-08' AS Date), 4)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (59, 5, 149000.0000, N'COD', N'rshfxb12414', CAST(N'2021-11-08' AS Date), 23544353)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (60, 5, 517000.0000, N'MOMO', N'Ninh Bình', CAST(N'2021-11-09' AS Date), 888195313)
+INSERT [dbo].[bill] ([bill_id], [user_id], [total], [payment], [address], [date], [phone]) VALUES (61, 5, 139000.0000, N'VNPAY', N'ưgrs', CAST(N'2021-11-09' AS Date), 32)
+SET IDENTITY_INSERT [dbo].[bill] OFF
+GO
+SET IDENTITY_INSERT [dbo].[bill_detail] ON
+
+INSERT [dbo].[bill_detail] ([detail_id], [bill_id], [product_id], [quantity], [price]) VALUES (40, 45, N'ISBN123456', 1, 367000)
+INSERT [dbo].[bill_detail] ([detail_id], [bill_id], [product_id], [quantity], [price]) VALUES (41, 45, N'ISBN789012', 2, 471000)
+INSERT [dbo].[bill_detail] ([detail_id], [bill_id], [product_id], [quantity], [price]) VALUES (42, 46, N'ISBN789012', 2, 471000)
+INSERT [dbo].[bill_detail] ([detail_id], [bill_id], [product_id], [quantity], [price]) VALUES (43, 47, N'ISBN345678', 2, 431000)
+INSERT [dbo].[bill_detail] ([detail_id], [bill_id], [product_id], [quantity], [price]) VALUES (44, 47, N'ISBN901234', 2, 299000)
+
+SET IDENTITY_INSERT [dbo].[bill_detail] OFF
+go
+SET IDENTITY_INSERT [dbo].[category] ON 
+
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (1, N'Fiction')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (2, N'Non-Fiction')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (3, N'Science Fiction')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (4, N'Mystery')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (5, N'Thriller')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (6, N'Romance')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (7, N'Fantasy')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (8, N'Biography')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (9, N'History')
+INSERT [dbo].[category] ([category_id], [category_name]) VALUES (10, N'Self-Help');
+SET IDENTITY_INSERT [dbo].[category] OFF
+go
+
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN123456', 1, N'To Kill a Mockingbird', 367000, N'A classic novel by Harper Lee', 50, N'images/book1.jpg')
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN789012', 2, N'Educated: A Memoir', 471000, N'A memoir by Tara Westover', 30, N'images/book2.jpg')
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN345678', 3, N'Dune', 431000, N'A science fiction novel by Frank Herbert', 25, N'images/book3.jpg')
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN901234', 4, N'The Da Vinci Code', 299000, N'A mystery thriller novel by Dan Brown', 40, N'images/book4.jpg')
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN567890', 5, N'Gone Girl', 327000, N'A thriller novel by Gillian Flynn', 35, N'images/book5.jpg')
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN234567', 6, N'Pride and Prejudice', 253000, N'A romantic novel by Jane Austen', 55, N'images/book6.jpg')
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN890123', 7, N'Harry Potter and the Sorcerer''s Stone', 459000, N'A fantasy novel by J.K. Rowling', 60, N'images/book7.jpg')
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN456789', 8, N'Steve Jobs', 379000, N'A biography of Steve Jobs by Walter Isaacson', 20, N'images/book8.jpg')
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN012345', 9, N'Sapiens: A Brief History of Humankind', 528000, N'A history book by Yuval Noah Harari', 45, N'images/book9.jpg')
+INSERT [dbo].[product] ([product_id], [category_id], [product_name], [product_price], [product_describe], [quantity], [img]) VALUES (N'ISBN678901', 10, N'The Subtle Art of Not Giving a F*ck', 317000, N'A self-help book by Mark Manson', 65, N'images/book10.jpg')
+
+
+GO
+SET IDENTITY_INSERT [dbo].[users] ON 
+
+INSERT [dbo].[users] ([user_id], [user_name], [user_email], [user_pass], [isAdmin]) VALUES (1, N'Admin', N'khuonghung1423@gmail.com', N'khuonghung1423', N'TRUE')
+INSERT [dbo].[users] ([user_id], [user_name], [user_email], [user_pass], [isAdmin]) VALUES (2, N'Hùng', N'khuonghung', N'123456', N'FALSE')
+INSERT [dbo].[users] ([user_id], [user_name], [user_email], [user_pass], [isAdmin]) VALUES (3, N'Hương', N'thuyhuong1423@gmail.com', N'123456', N'FALSE')
+INSERT [dbo].[users] ([user_id], [user_name], [user_email], [user_pass], [isAdmin]) VALUES (4, N'Khuong Hung', N'khuonghung2408@gmail.com', N'1423', N'FALSE')
+INSERT [dbo].[users] ([user_id], [user_name], [user_email], [user_pass], [isAdmin]) VALUES (5, N'Mai Phạm', N'maipham11@gmail.com', N'123', N'FALSE')
+INSERT [dbo].[users] ([user_id], [user_name], [user_email], [user_pass], [isAdmin]) VALUES (6, N'Arya', N'hre@yahoo.com', N'1245', N'FALSE')
+SET IDENTITY_INSERT [dbo].[users] OFF
+GO
+ALTER TABLE [dbo].[bill]  WITH CHECK ADD  CONSTRAINT [FK_bill_users] FOREIGN KEY([user_id])
+REFERENCES [dbo].[users] ([user_id])
+GO
+ALTER TABLE [dbo].[bill] CHECK CONSTRAINT [FK_bill_users]
+GO
+ALTER TABLE [dbo].[bill_detail]  WITH CHECK ADD  CONSTRAINT [FK_bill_detail_bill] FOREIGN KEY([bill_id])
+REFERENCES [dbo].[bill] ([bill_id])
+GO
+ALTER TABLE [dbo].[bill_detail] CHECK CONSTRAINT [FK_bill_detail_bill]
+GO
+ALTER TABLE [dbo].[bill_detail]  WITH CHECK ADD  CONSTRAINT [FK_bill_detail_product] FOREIGN KEY([product_id])
+REFERENCES [dbo].[product] ([product_id])
+GO
+ALTER TABLE [dbo].[bill_detail] CHECK CONSTRAINT [FK_bill_detail_product]
+GO
+ALTER TABLE [dbo].[cart]  WITH CHECK ADD  CONSTRAINT [FK_cart_product] FOREIGN KEY([product_id])
+REFERENCES [dbo].[product] ([product_id])
+GO
+ALTER TABLE [dbo].[cart] CHECK CONSTRAINT [FK_cart_product]
+GO
+ALTER TABLE [dbo].[product]  WITH CHECK ADD  CONSTRAINT [FK_product_category] FOREIGN KEY([category_id])
+REFERENCES [dbo].[category] ([category_id])
+GO
+ALTER TABLE [dbo].[product] CHECK CONSTRAINT [FK_product_category]
+GO
